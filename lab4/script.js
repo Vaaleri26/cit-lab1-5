3c641f188c5900aa7f7ddd63c87db61585bad85c
Vaaleri26 <93161063+Vaaleri26@users.noreply.github.com>
2021-11-09 16:32:53 +0300

    Update script.js

--- lab4/script.js
+++ lab4/script.js
@@ -1,50 +1,39 @@
-const container = document.querySelector(".container");
-const preloader = document.querySelector(".preloader");
-
-const sex = {
-  male: "Мужской",
-  female: "Женский"
-};
-
 const getData = async () => {
   const repsonse = await fetch("https://randomuser.me/api/?results=10", {
     method: "GET"
   });
-
   const data = await repsonse.json();
 
-  preloader.style.display = "none";
+  let container = document.querySelector(".container");
+  let preloader = document.querySelector(".preloader");
 
+  preloader.style.display = "none";
+  const sex = {
+    male: "Мужской",
+    female: "Женский"
+  };
   data.results.forEach((el) => {
     let card = document.createElement("div");
     card.setAttribute("class", "card");
     card.innerHTML = `
                 <div class="form">
-                <p class="inf">Имя и фамилия:<span class="polzt">${
-                  el.name.title
-                } ${el.name.first} ${el.name.last}</span></p>
+                <p>Имя и фамилия:${el.name.title} ${el.name.first} ${
+      el.name.last
+    }</p>
               
-                    <p class="inf">Пол: <span class="polzt">${
+                    <p>Пол: <span>${
                       el.gender === "male" ? sex.male : sex.female
                     }</span></p>
-                    <p class="inf">Адрес: <span class="polz">${
-                      el.location.country
-                    } ${el.location.state} ${el.location.city} ${
-      el.location.street.name
-    }, ${el.location.street.number}</span></p>
-                    <p class="inf">Email: <span class="polz">${
-                      el.email
-                    }</span></p>
-                    <p class="inf">Дата рождения: <span class="polz">${el.dob.date
+                    <p>Адрес: ${el.location.country} ${el.location.state} ${
+      el.location.city
+    } ${el.location.street.name}, ${el.location.street.number}</p>
+                    <p>Email:${el.email}</p>
+                    <p>Дата рождения: ${el.dob.date
                       .substring(0, 10)
                       .split("-")
-                      .join("-")}</span></p>
-                    <p class="inf">Возраст: <span class="polz">${
-                      el.dob.age
-                    }</span></p>
-                    <p class="inf">Телефон: <span class="polz">${
-                      el.phone
-                    }</span></p>
+                      .join("-")}</p>
+                    <p>Возраст:${el.dob.age}</p>
+                    <p >Телефон:${el.phone}</p>
                     <img src=${el.picture.medium} alt="">
                     </div>
                     `;
