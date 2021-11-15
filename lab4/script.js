21fe471e05a81545063a110518ba15059ca7f308
Vaaleri26 <93161063+Vaaleri26@users.noreply.github.com>
2021-11-15 14:37:57 +0300

    Update script.js

--- lab4/script.js
+++ lab4/script.js
@@ -1,5 +1,5 @@
 const getData = async () => {
-  const repsonse = await fetch("https://randomuser.me/api/?results=10", {
+  let repsonse = await fetch("https://randomuser.me/api/?results=10", {
     method: "GET"
   });
   const data = await repsonse.json();
@@ -8,7 +8,7 @@ const getData = async () => {
   let preloader = document.querySelector(".preloader");
 
   preloader.style.display = "none";
-  const sex = {
+  const gender = {
     male: "Мужской",
     female: "Женский"
   };
@@ -16,22 +16,15 @@ const getData = async () => {
     let card = document.createElement("div");
     card.setAttribute("class", "card");
     card.innerHTML = `
-                <div class="form">
-                <p>Имя и фамилия:${el.name.title} ${el.name.first} ${
-      el.name.last
-    }</p>
-              
-                    <p>Пол: <span>${
-                      el.gender === "male" ? sex.male : sex.female
-                    }</span></p>
-                    <p>Адрес: ${el.location.country} ${el.location.state} ${
-      el.location.city
-    } ${el.location.street.name}, ${el.location.street.number}</p>
+ <div class="form">
+<p>Имя и фамилия:${el.name.title} ${el.name.first} ${el.name.last}</p>
+<p>Пол: ${el.gender === "male" ? gender.male : gender.female}</p>
+<p>Адрес: ${el.location.country} ${el.location.state} ${el.location.city} ${
+      el.location.street.name
+    }, ${el.location.street.number}</p>
                     <p>Email:${el.email}</p>
                     <p>Дата рождения: ${el.dob.date
-                      .substring(0, 10)
-                      .split("-")
-                      .join("-")}</p>
+                      .substring(0, 10)}</p>
                     <p>Возраст:${el.dob.age}</p>
                     <p >Телефон:${el.phone}</p>
                     <img src=${el.picture.medium} alt="">
